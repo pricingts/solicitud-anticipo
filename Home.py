9ff98debc7a3b669a96490d71e44f7ee9063fd6a
Trading Solutions <tradingsolutions@MacBook-Air-de-Trading.local>
2025-04-25 10:18:36 -0500

    emails

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ def identity_role(email):
         "sales5@tradingsol.com", "bds@tradingsol.com", "insidesales@tradingsol.com", "sales6@tradingsol.com", "sales5@tradingsol.com"
     ]
 
-    inside =["pricing7@tradingsolutions.com", "traffic2@traingsolutions.com", "pricing7@tradingsol.com", "traffic2@traingsol.com"]
+    inside =["pricing7@tradingsolutions.com", "traffic2@traingsolutions.com", "customer3@tradingsolutions.com", "trainer@tradingsolutions.com"]
 
     if email in admin:
         return "admin"
